9e8ca9fba488a28b42ee42fd1232cddc0bdaf903
Robert Spatz <bspatz2@huskers.unl.edu>
2021-08-26 14:34:57 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: bob
+ *
+ * Date: 2021/8/24
+ *
+ * A simple not a hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("bobby spatz\n");
+
+  return 0;
+}

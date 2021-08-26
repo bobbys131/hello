dbd4b8bb199a6f916babdf2263fbdc4812bdb759
Logan Trimpey <ltrimpey2@huskers.unl.edu>
2021-08-26 15:23:14 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@
  *
  * A simple not a hello world program in C
  *
+ * Contributor: Logan Trimpey
  */
 #include <stdlib.h>
 #include <stdio.h>
@@ -12,6 +13,9 @@
 int main(int argc, char **argv) {
 
   printf("bobby spatz!\n");
-  printf("computer science\n")
+  printf("computer science\n\n");
+  
+  printf("How are you today?\n");
+  printf("It's a shame I only get two lines I could have actually had you say something\n");
   return 0;
 }
